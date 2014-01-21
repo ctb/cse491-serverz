026291aea3d344be55f82f069482af6ae22db651
Maxwell Brown <brown308@msu.edu>
2014-01-20 19:04:25 -0500

    hw1 corrections

--- server.py
+++ server.py
@@ -8,6 +8,10 @@ host = socket.getfqdn() # Get local machine name
 port = random.randint(8000, 9999)
 s.bind((host, port))        # Bind to the port
 
+http_response =  'HTTP/1.0 200 OK \r\n'
+content_type = 'Content-Type:text/html \r\n'
+html_text = '<h1>Hello World</h1> this is MaxwellGBrown\'s Web server.'
+
 print 'Starting server on', host, port
 print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
@@ -19,10 +23,10 @@ while True:
     c, (client_host, client_port) = s.accept()
     print c.recv(1000)
     print 'Got connection from', client_host, client_port
-    c.send('''HTTP/1.0 200 OK
-Content-Type:text/html
-
-<h1>Hello World</h1> this is MaxwellGBrown's Web server.''')
+    c.send(http_response)
+    c.send(content_type)
+    c.send('\r\n')
+    c.send(html_text)
 # @You could move this string into a veriable during initialization
 #  and just send the text string
     c.close()
