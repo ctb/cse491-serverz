c6bbbf316a3d683e96305580633355ed51940c00
Maxwell Brown <brown308@msu.edu>
2014-01-20 21:43:44 -0500

    hw2 - first attempt

--- server.py
+++ server.py
@@ -3,30 +3,102 @@ import random
 import socket
 import time
 
-s = socket.socket()         # Create a socket object
-host = socket.getfqdn() # Get local machine name
-port = random.randint(8000, 9999)
-s.bind((host, port))        # Bind to the port
+    
+def main():
+    s = socket.socket()         # Create a socket object
+    host = socket.getfqdn() # Get local machine name
+    port = random.randint(8000, 9999)
+    s.bind((host, port))        # Bind to the port
 
-http_response =  'HTTP/1.0 200 OK \r\n'
-content_type = 'Content-Type:text/html \r\n'
-html_text = '<h1>Hello World</h1> this is MaxwellGBrown\'s Web server.'
+    print 'Starting server on', host, port
+    print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
-print 'Starting server on', host, port
-print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+    s.listen(5)                 # Now wait for client connection.
 
-s.listen(5)                 # Now wait for client connection.
+    print 'Entering infinite loop; hit CTRL-C to exit'
+    while True:
 
-print 'Entering infinite loop; hit CTRL-C to exit'
-while True:
-    # Establish connection with client.    
-    c, (client_host, client_port) = s.accept()
-    print c.recv(1000)
-    print 'Got connection from', client_host, client_port
-    c.send(http_response)
-    c.send(content_type)
-    c.send('\r\n')
-    c.send(html_text)
-# @You could move this string into a veriable during initialization
-#  and just send the text string
-    c.close()
+        # Ctrl+C KeyboardInterrupt error handler
+        try:
+            # Establish connection with client.
+            c, (client_host, client_port) = s.accept()
+            print 'Got connection from', client_host, client_port
+##            print c.recv(1000)
+            handle_connection(c)
+        except (KeyboardInterrupt):
+            print "\r\nEnding server.py ...\r\n"
+            exit(0)
+            
+
+
+
+def handle_connection(conn):
+
+    ## handle request data
+    request_data = conn.recv(1000)
+    request_type = request_data.splitlines()[0].rsplit(' ')[0] # prints the page requested (1st line, 1st word)
+    page_request = request_data.splitlines()[0].rsplit(' ')[1] # prints the page requested (1st line, 2nd word)
+
+    print request_type + ' ' + page_request
+
+    ## initialize html_text
+    html_text = str()
+
+    ## set http_response
+    ## don't send the http_response just yet, incase page doesn't exist
+    http_response =  'HTTP/1.0 200 OK\r\n'
+    content_type = 'Content-type: text/html\r\n'
+
+
+    ## handle different types of HTML requests
+    if request_type == "GET":
+        ## serve HTML content
+        if page_request == '/':
+            ## cse.msu.edu:xxxx/
+            html_text = '<h1>Hello, world.</h1>' + \
+                        'This is MaxwellGBrown\'s Web server.' + \
+                        '<ul>' + \
+                        '<li><a href="/content">content</a></li>' + \
+                        '<li><a href="/file">file</a></li>' + \
+                        '<li><a href="/image">image</a></li>' + \
+                        '</ul>'
+            
+        elif page_request == '/content':
+            ## cse.msu.edu:xxx/content
+            html_text = '<h1>Hello, world.</h1>' + \
+                        'This is MaxwellGBrown\'s Web server.' + \
+                        '<h2>Content</h2>'
+            
+        elif page_request == '/file':
+            ## cse.msu.edu:xxx/file
+            html_text = '<h1>Hello, world.</h1>' + \
+                        'This is MaxwellGBrown\'s Web server.' + \
+                        '<h2>File</h2>'
+            
+        elif page_request == '/image':
+            ## cse.msu.edu:xxx/image
+            html_text = '<h1>Hello, world.</h1>' + \
+                        'This is MaxwellGBrown\'s Web server.' + \
+                        '<h2>Image</h2>'
+
+    elif request_type == "POST":
+        ## POST request handler
+        html_text = '<h1>Hello, world.</h1>' + \
+                    'MaxwellGBrown\'s POST request HTML.'
+
+
+
+    
+    ## send http response and content
+    conn.send(http_response)
+    conn.send(content_type)
+    conn.send('\r\n')
+    conn.send(html_text)
+    conn.close()
+
+
+
+
+## Run main
+if __name__ == '__main__':
+    main()
--- test_server.py
+++ test_server.py
@@ -28,13 +28,59 @@ class FakeConnection(object):
 # Test a basic GET call.
 
 def test_handle_connection():
-    conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
-    expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello, world.</h1>' + \
-                      'This is ctb\'s Web server.'
+##    conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
+##    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+##                      'Content-type: text/html\r\n' + \
+##                      '\r\n' + \
+##                      '<h1>Hello, world.</h1>' + \
+##                      'This is MaxwellGBrown\'s Web server.' + \
 
+    ## Do a handle_connection for all 4 pages                
+    for page in ['/','/content','/file','/image']:
+        conn = FakeConnection("GET "+page+" HTTP/1.0\r\n\r\n")
+
+        ## standardized info per page
+        expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                          'Content-type: text/html\r\n' + \
+                          '\r\n' + \
+                          '<h1>Hello, world.</h1>' + \
+                          'This is MaxwellGBrown\'s Web server.'
+
+        #Add content to check for per page
+        if page == '/':
+            expected_return = expected_return + \
+                              '<ul>' + \
+                              '<li><a href="/content">content</a></li>' + \
+                              '<li><a href="/file">file</a></li>' + \
+                              '<li><a href="/image">image</a></li>' + \
+                              '</ul>'
+        elif page == '/content':
+            expected_return = expected_return + \
+                              '<h2>Content</h2>'
+
+        elif page == '/file':
+            expected_return = expected_return + \
+                              '<h2>File</h2>'
+
+        elif page == '/image':
+            expected_return = expected_return + \
+                              '<h2>Image</h2>'
+                              
+                          
+    
+        server.handle_connection(conn)
+
+        assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_handle_connection_POST():
+    conn = FakeConnection("POST / HTTP/1.0\r\n\r\n")
+    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                          'Content-type: text/html\r\n' + \
+                          '\r\n' + \
+                          '<h1>Hello, world.</h1>' + \
+                          'MaxwellGBrown\'s POST request HTML.'
+    
     server.handle_connection(conn)
 
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    
