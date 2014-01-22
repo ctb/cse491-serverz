0f574d418fb5c902650b7dca3339f83714fdc6a5
fakestuff <315242904@qq.com>
2014-01-22 18:40:39 -0500

    hw2

--- server.py
+++ server.py
@@ -3,24 +3,30 @@ import random
 import socket
 import time
 
-s = socket.socket()         # Create a socket object
-host = socket.getfqdn() # Get local machine name
-port = random.randint(8000, 9999)
-s.bind((host, port))        # Bind to the port
+def main():
+    s = socket.socket()         # Create a socket object
+    host = socket.getfqdn() # Get local machine name
+    port = random.randint(8000, 9999)
+    s.bind((host, port))        # Bind to the port
 
-print 'Starting server on', host, port
-print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+    print 'Starting server on', host, port
+    print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
-s.listen(5)                 # Now wait for client connection.
+    s.listen(5)                 # Now wait for client connection.
+    print 'Entering infinite loop; hit CTRL-C to exit'
+    while True:
+        handle_connection(s)
 
-print 'Entering infinite loop; hit CTRL-C to exit'
-while True:
-    # Establish connection with client.    
-    c, (client_host, client_port) = s.accept()
-    print 'Got connection from', client_host, client_port
-    c.send('HTTP/1.0 200 OK\r\n')
-    c.send('Content-Type: text/html\r\n')
-    c.send('\r\n')
-    c.send("<h1>Hello,world</h1>")
-    c.send("this is fakestuff's WebServer.")
-    c.close()
+def handle_connection(coon):
+        # Establish connection with client.    
+        c, (client_host, client_port) = coon.accept()
+        print 'Got connection from', client_host, client_port
+        c.send('HTTP/1.0 200 OK\r\n')
+        c.send('Content-Type: text/html\r\n')
+        c.send('\r\n')
+        c.send("<h1>Hello,world</h1>")
+        c.send("this is fakestuff's WebServer.")
+        c.close()
+
+if __name__ == '__main__':
+    main()
