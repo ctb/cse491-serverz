46558dfdf1e1988b4c52a2bccb688fb5b7dad8b6
Cameron Keif <keifcame@arctic.cse.msu.edu>
2014-01-22 12:23:51 -0500

    [14~Updated tests file to check the responses for /content, /file, and /image

--- test_server.py
+++ test_server.py
@@ -27,13 +27,55 @@ class FakeConnection(object):
 
 # Test a basic GET call.
 
+# Test path = /
 def test_handle_connection():
     conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
     expected_return = 'HTTP/1.0 200 OK\r\n' + \
                       'Content-type: text/html\r\n' + \
                       '\r\n' + \
-                      '<h1>Hello, world.</h1>' + \
-                      'This is keifcame\'s Web server.'
+                      '<h1>Links to other pages</h1>' + \
+                      '<a href = /content>Content</a><br>' + \
+                      '<a href = /file>File</a><br>' + \
+                      '<a href = /image>Image</a>'
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+# Test path = /content
+def test_handle_connection_content():
+    conn = FakeConnection("GET /content HTTP/1.0\r\n\r\n")
+    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                      'Content-type: text/html\r\n' + \
+                      '\r\n' + \
+                      '<h1>Cam is great</h1>' + \
+                      'This is some content.'
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+# Test path = /file
+def test_handle_connection_file():
+    conn = FakeConnection("GET /file HTTP/1.0\r\n\r\n")
+    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                      'Content-type: text/html\r\n' + \
+                      '\r\n' + \
+                      '<h1>They don\'t think it be like it is, but it do.</h1>' + \
+                      'This some file.'
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+# Test path = /content
+def test_handle_connection_image():
+    conn = FakeConnection("GET /image HTTP/1.0\r\n\r\n")
+    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                      'Content-type: text/html\r\n' + \
+                      '\r\n' + \
+                      '<h1>Wow. Such page. Very HTTP response</h1>' + \
+                      'This is some image.'
 
     server.handle_connection(conn)
 
