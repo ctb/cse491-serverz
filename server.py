cd9c552f323bb15bb692e91c8122c045ea0cb7c9
Jourdan West <westjour@arctic.cse.msu.edu>
2014-01-23 10:23:20 -0500

    mychanges

--- server.py
+++ server.py
@@ -28,8 +28,6 @@ def main():
   
   return
 
-  #s.close()
-
 def handle_connection(conn):
 	request = conn.recv(1000)
 	method = request.split('\n')[0].split(' ')[0]
@@ -40,25 +38,25 @@ def handle_connection(conn):
 		
 	# send a response
 	if method == 'POST':
-		    conn.send('hello world')
-		    return
+		conn.send('hello world')
+		return
              
 	elif path == '/':
-		    conn.send('<h1>Hello World!</h1>')
-		    conn.send('This is westjour\'s web server.')
-		    conn.send('<br>')
-		    conn.send('<a href="/content">Content</a><br />')
-		    conn.send('<a href="/content">File</a><br />')
-		    conn.send('<a href="/content">Image</a><br />')
+		conn.send('<h1>Hello World!</h1>')
+		conn.send('This is westjour\'s web server.')
+		conn.send('<br>')
+		conn.send('<a href="/content">Content</a><br />')
+		conn.send('<a href="/content">File</a><br />')
+		conn.send('<a href="/content">Image</a><br />')
 		    
 	elif path == '/content':
-		    conn.send('<h1>This is westjour\'s content page.</h1>')
+		conn.send('<h1>This is westjour\'s content page.</h1>')
 		    
 	elif path == '/file':
-		    conn.send('<h1>This is westjour\'s file page.</h1>')
+		conn.send('<h1>This is westjour\'s file page.</h1>')
 
 	elif path == '/image':
-		  conn.send('<h1>This is westjour\'s image page.</h1>')
+		conn.send('<h1>This is westjour\'s image page.</h1>')
 
 	conn.close()
 		
