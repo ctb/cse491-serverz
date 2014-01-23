b4a0bed42e421c732ef923619986c884aeba43d6
Jourdan West <westjour@arctic.cse.msu.edu>
2014-01-23 10:20:04 -0500

    mychanges

--- server.py
+++ server.py
@@ -31,38 +31,38 @@ def main():
   #s.close()
 
 def handle_connection(conn):
-		request = conn.recv(1000)
-		method = request.split('\n')[0].split(' ')[0]
-		path = request.split('\n')[0].split(' ')[1]
-		conn.send('HTTP/1.0 200 OK\r\n')
-		conn.send('Content-type: text/html\r\n')
-		conn.send('\r\n')
+	request = conn.recv(1000)
+	method = request.split('\n')[0].split(' ')[0]
+	path = request.split('\n')[0].split(' ')[1]
+	conn.send('HTTP/1.0 200 OK\r\n')
+	conn.send('Content-type: text/html\r\n')
+	conn.send('\r\n')
 		
-		# send a response
-		if method == 'POST':
-       conn.send('hello world')
-       return
+	# send a response
+	if method == 'POST':
+		    conn.send('hello world')
+		    return
              
-		elif path == '/':
-       conn.send('<h1>Hello World!</h1>')
-       conn.send('This is westjour\'s web server.')
-       conn.send('<br>')
-       conn.send('<a href="/content">Content</a><br />')
-       conn.send('<a href="/content">File</a><br />')
-       conn.send('<a href="/content">Image</a><br />')
-       
-		elif path == '/content':
-       conn.send('<h1>This is westjour\'s content page.</h1>')
-       
-		elif path == '/file':
-       conn.send('<h1>This is westjour\'s file page.</h1>')
+	elif path == '/':
+		    conn.send('<h1>Hello World!</h1>')
+		    conn.send('This is westjour\'s web server.')
+		    conn.send('<br>')
+		    conn.send('<a href="/content">Content</a><br />')
+		    conn.send('<a href="/content">File</a><br />')
+		    conn.send('<a href="/content">Image</a><br />')
+		    
+	elif path == '/content':
+		    conn.send('<h1>This is westjour\'s content page.</h1>')
+		    
+	elif path == '/file':
+		    conn.send('<h1>This is westjour\'s file page.</h1>')
 
-    elif path == '/image':
-       conn.send('<h1>This is westjour\'s image page.</h1>')
+	elif path == '/image':
+		  conn.send('<h1>This is westjour\'s image page.</h1>')
 
-    conn.close()
+	conn.close()
 		
-		return
+	return
 
 
 
