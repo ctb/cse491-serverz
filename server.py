f7abb445ef38c751876f4c2c19ea728005ed7b30
fakestuff <315242904@qq.com>
2014-01-30 10:53:16 -0500

    hw3 beta1

--- server.py
+++ server.py
@@ -21,30 +21,51 @@ def main():
         #Establish connection with client.
        # url = host + ':' +str(port)
         handle_connection(c)
+def handle_submit(para,conn):
+    path = para.split('\r\n')[0].split(' ')[1]
+    print path
+    first_name = path.split('firstname=')[1].split('&lastname=')[0]
+    last_name = path.split('firstname=')[1].split('&lastname=')[1]
+    print first_name
+    print last_name
+    conn.send('Hello Mr. %s %s' % (first_name, last_name))
 
 def handle_connection(conn):
-        command = conn.recv(1000).split('\r\n')[0].split(' ')[0:2]
+        receive = conn.recv(1000)
+        print receive       
+        command = receive.split('\r\n')[0].split(' ')[0:2]
         method = command[0]
-        req = command[1]
+        req = command[1].split('?')[0]
+        print req
         conn.send('HTTP/1.0 200 OK\r\n')
         conn.send('Content-type: text/html\r\n')
         conn.send('\r\n')
         if method == 'POST':
-           conn.send('<h1>Hello, world.</h1>')
+            if req == '/submit':
+                handle_submit(para,conn)
+            else:
+                conn.send('<h1>Hello, world.</h1>')
+                conn.sned('')
         elif method == 'GET':
-           if req == '/':
-               conn.send("<h1>Hello, world.</h1>")
-               conn.send("This is fakestuff's Web server.")
-               conn.send("There is a list of 'website' available.<br>")
-               conn.send('<a href= /content>Content</a><br>')
-               conn.send('<a href= /file>File</a><br>')
-               conn.send('<a href= /image>Image</a><br>')
-           if req == '/content':
-               conn.send("This is the content you want to see.")
-           if req == '/file':
-               conn.send("There will be some files in future.")
-           if req == '/image':
-               conn.send("Only most smart guy could see the image. LOL")  
+            if req == '/':
+                conn.send("<h1>Hello, world.</h1>")
+                conn.send("This is fakestuff's Web server.")
+                conn.send("There is a list of 'website' available.<br>")
+                conn.send('<a href= /content>Content</a><br>')
+                conn.send('<a href= /file>File</a><br>')
+                conn.send('<a href= /image>Image</a><br>')
+                conn.send("<form action='/submit' method='GET'>")
+                conn.send("<input type='text' name='firstname'>")
+                conn.send("<input type='text' name='lastname'>")
+                conn.send("<input type='submit' value='Submit'></form>")
+            if req == '/content':
+                conn.send("This is the content you want to see.")
+            if req == '/file':
+                conn.send("There will be some files in future.")
+            if req == '/image':
+                conn.send("Only most smart guy could see the image. LOL")
+            if req == '/submit':
+                handle_submit(receive,conn)
         conn.close()
 
 if __name__ == '__main__':
